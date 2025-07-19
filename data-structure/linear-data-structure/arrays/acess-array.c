92ca2034cb7484c7bb83e962c47e9add76cf6309
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-07-19 10:09:23 -0300

    feat: Inserindo estruturas de array em C

---
+++ data-structure/linear-data-structure/arrays/acess-array.c
@@ -0,0 +1,19 @@
+/*
+binary posicitions
+
+posictions in computer starts in 0. If i have a array with 4 elements, the firts one is 0, the second is 1, the thrist is 2 
+and the fourth is 3 
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int arr[3] = {1, 2, 3};
+
+    printf("%d\n", arr[0]);
+    printf("%d\n", arr[1]);
+    printf("%d\n", arr[2]);
+
+    return 0;
+}
---
+++ data-structure/linear-data-structure/arrays/declaration-array.c
@@ -0,0 +1,25 @@
+/*
+declaration types of arrays
+
+Arrays are declarated in 3 type forms, one-dimensional, multi-dimensional(matrix) and three-dimensional
+
+1D: int arr[];
+2D: int arr[][];
+3D: int arr[][][];
+
+Is possible to inicialized arrys with repetition structure, like FOR.
+*/
+
+# include <stdio.h>
+
+int main(){
+    //inicialize methods 
+    int a[3] = {1, 2, 3};
+    
+    int b[] = {1, 2, 3, 4};
+    
+    int c[1];
+    c[1] = 2;
+
+    return 0;
+}
