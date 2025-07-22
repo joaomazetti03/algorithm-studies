bcb4d63f7b9d2eeb06fba7adf5a0e38f25879a69
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-07-21 22:48:43 -0300

    refactor(binarysearch): Refatorando o algoritmo de busca binária na linguagem C

--- data-structure/search/binarysearch.c
+++ data-structure/search/binarysearch.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+void binarysearch();
+
 int main(){
     printf("Hello World!");
 
     return 0;
+}
+
+void binarysearch(list, iten){
+    int left = 0;
+    int right = sizeof(list) - 1;
+
+    while(left <= right){
+        int mid = (left + right) % 2;
+        
+    }
 }
